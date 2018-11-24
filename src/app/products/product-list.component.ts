da6299c3eb939b710c724e41175f30d1c68ada20
Gilberto Martini Madeira <gilberto.madeira@outlook.com>
2018-11-24 01:25:53 -0200

    adiciona o binding de estilo na imagem

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle: String = 'Product list';
+    imageWidth = 50;
+    imageMargin = 2;
     products: any[] = [
         {
           'productId': 1,
